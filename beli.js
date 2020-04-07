fefec478cb50b5219441e5d88d27c43ad8a4dafd
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-04-07 12:31:05 +0700

    done

--- beli.js
+++ beli.js
@@ -1,15 +1,77 @@
-function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
-  setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
-      cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(uang)
-    }
-  }, obj.waktu);
+function belanja(){ // list of belanjaan
+  function beliNasdang(uang, cb){ // with default cb param
+  let obj = { 
+    item: 'Nasi Padang',
+    harga: 15000,
+    banyak: 2
+  }
+  uang -= (obj.harga * obj.banyak)
+  console.log(`Sisa uang belanja ${obj.item} adalah Rp${uang},-`)
+  cb(uang)
 }
 
-module.exports = beli;
+function beliNasgor(sisaUang, cb){
+  let obj = {
+    item: 'Nasi Goreng',
+    harga: 10000,
+    banyak: 5
+  }
+  sisaUang -= (obj.harga * obj.banyak)
+  console.log(`Sisa uang belanja ${obj.item} adalah Rp${sisaUang},-`)
+  cb(sisaUang)
+}
+
+function beliNasduk(sisaUang, cb){
+  let obj = {
+    item: 'Nasi Uduk',
+    harga: 5000,
+    banyak: 5
+  }
+  sisaUang -= (obj.harga * obj.banyak)
+  console.log(`Sisa uang belanja ${obj.item} adalah Rp${sisaUang},-`)
+  cb(sisaUang)
+}
+
+function beliMiegor(sisaUang, cb){
+  let obj = {
+    item: 'Mie Goreng',
+    harga: 9500,
+    banyak: 3
+  }
+  sisaUang -= (obj.harga * obj.banyak)
+  console.log(`Sisa uang belanja ${obj.item} adalah Rp${sisaUang},-`)
+  cb(sisaUang)
+}
+
+function beliMiereb(sisaUang, cb){
+  let obj = {
+    item: 'Mie Rebus',
+    harga: 9500,
+    banyak: 7
+  }
+  sisaUang -= (obj.harga * obj.banyak)
+  console.log(`Sisa uang belanja ${obj.item} adalah Rp${sisaUang},-`)
+  cb(sisaUang)
+}
+
+let uang = 200001
+
+
+// drive code
+console.log(`saya mempunyai uang ${uang}`)
+beliNasdang(uang, (sisaUang)=>{
+  beliNasgor(sisaUang, (sisaUang)=>{
+    beliNasduk(sisaUang, (sisaUang)=>{
+      beliMiegor(sisaUang, (sisaUang)=>{
+        beliMiereb(sisaUang, ()=>{
+
+        })
+      })
+    })
+  })
+})
+}
+
+// summon function
+belanja();
+module.exports = belanja;
---
+++ index.js
@@ -0,0 +1,2 @@
+const belanja = require('./beli');
+
