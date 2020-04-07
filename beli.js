2734715e31fa19a91e77c4233290cfe6f6b3898b
Agustinus Adam <agustinusadamw@gmail.com>
2020-04-07 12:42:36 +0700

    callback-belanja done

--- beli.js
+++ beli.js
@@ -2,11 +2,11 @@ function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (uang > obj.harga) {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
+    }else {
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${uang}`);
       cb(uang)
     }
   }, obj.waktu);
